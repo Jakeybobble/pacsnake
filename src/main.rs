10e4db393dce74892b37fb5fe6296b8905fb5b9a
Jacob G.S. <jacobgsdev@gmail.com>
2025-12-19 09:29:19 +0100

    Optimizations, wasm build target

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
+            fit_canvas_to_parent: true,
             title: "Pac-snake".to_string(),
             ..default()
         }),
         ..default()
     }));
+
+    app.insert_resource(ClearColor(Color::BLACK));
 
     app.run();
 }
